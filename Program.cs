145d90593bfc25658f8a5c026380706ec8f565d8
Menaid <menaidnocic@live.se>
2023-09-22 11:27:28 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * 3 --> Fizz
+ * 5 --> Buzz
+ * 3 & 5 --> FizzBuzz
+ * 7 --> Bazz
+ * 3 & 7 --> FizzBazz (21)
+ * 5 & 7 --> BuzzBazz (35)
+ * 3 & 5 & 7 --> FizzBuzzBazz (105)
+ */
+
+/*
+
+var n = 1;
+
+while (n != 151)
+{
+
+    if (n % 3 != 0 && n % 5 != 0 && n % 7 != 0 && n % 15 != 0 && n % 21 != 0 && n % 35 != 0 && n % 105 != 0)
+
+    {
+        Console.WriteLine(n);
+    }
+    else if (n % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (n % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else if (n % 7 == 0)
+    {
+        Console.WriteLine("Bazz");
+    }
+    else if (n % 15 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (n % 21 == 0)
+    {
+        Console.WriteLine("FizzBazz");
+    }
+    else if (n % 35 == 0)
+    {
+        Console.WriteLine("BuzzBazz");
+    }
+    else if (n % 105 == 0)
+    {
+        Console.WriteLine("FizzBuzzBazz");
+    }
+    else
+    {
+        Console.WriteLine(n);
+    }
+    
+    n = n + 1;
+}
+
+*/
+
